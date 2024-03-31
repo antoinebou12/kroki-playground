7ce52a16d6c4ca33fb973d13c67ea2fd84a8b96d
Antoine Boucher <antoine.bou13@gmail.com>
2024-03-31 17:35:03 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
-// next.config.js
 module.exports = {
-  // Add custom webpack configuration
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Add custom loader for SVG files
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
-
     return config;
   },
 
