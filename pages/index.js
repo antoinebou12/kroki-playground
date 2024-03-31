58e42087719a0e5fcccb7f0a1f023b54db95fd2a
Antoine Boucher <antoine.bou13@gmail.com>
2024-03-31 16:58:52 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DiagramGenerator from '../components/DiagramGenerator';
+
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Kroki Playground</h1>
+      <DiagramGenerator />
+    </div>
+  );
+};
+
+export default IndexPage;
